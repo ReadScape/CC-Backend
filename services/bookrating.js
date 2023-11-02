f565b5b5fe965761e4a3ee8cbedb19111d654f43
gbsf7 <3332200014@untirta.ac.id>
2023-11-02 10:39:55 +0700

    updated package.json

--- services/bookrating.js
+++ services/bookrating.js
@@ -5,7 +5,7 @@ const config = require('../config');
 async function getMultiple(page = 1){
   const offset = helper.getOffset(page, config.listPerPage);
   const rows = await db.query(
-    `SELECT user_id, fiction_id, click, rating FROM rating LIMIT ${offset},${config.listPerPage}`
+    `SELECT * FROM rating LIMIT ${offset},${config.listPerPage}`
   );
   const data = helper.emptyOrRows(rows);
   const meta = {page};
